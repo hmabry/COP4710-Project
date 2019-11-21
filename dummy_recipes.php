874ef02773da75f1f9af513da459608c6e515dc5
heaven <hfm16@my.fsu.edu>
2019-11-21 13:51:04 -0500

    dummy_recipes.php is for testing purposes, while index.php deals with user added recipes

--- dummy_recipes.php
+++ dummy_recipes.php
@@ -13,38 +13,52 @@
 	<?php
 	//Here I'm including the db connection file so I don't have to keep connecting in each file
 	include 'db_connection.php';
+	include 'scraper_setup.php';
 	
 	$db_found = mysqli_select_db($db_handle, $database);
 
 	if($db_found){
-		$SQL = "SELECT * FROM user";
+		$SQL = "SELECT * FROM recipe";
 		$result = mysqli_query($db_handle, $SQL);
 		
 		while ( $db_field = mysqli_fetch_assoc($result) ) {
 
 		print "<BR>";
-		print $db_field['u_id'] . "<BR>";
-		print $db_field['username'] . "<BR>";
-		print $db_field['password'] . "<BR>";
+		print $db_field['name'] . "<BR>";
+		print $db_field['author'] . "<BR>";
+		print $db_field['preptime'] . "<BR>";
 		}
 	}
 	else{
 		print "Database not found";
 	}
 	
-	$u_name = $_POST['user_name'];	
-
+	$name = ($recipe['name']);
+	$author = ($recipe['author']);
+	$preptime = ($recipe['cookTime']);
 	
-	$sql = "INSERT INTO user(username) VALUES ('$u_name')";
+	echo nl2br("\n");
+	print "the name is: ";
+	print $name;
+	echo nl2br("\n");
+	print "the author is: ";
+	print $author;
+	echo nl2br("\n");
+	print "the prep time is: ";
+	print $preptime;
+	echo nl2br("\n");
+	
+	#$sql = "INSERT INTO user(username) VALUES ('$author')";
+	$sql = "INSERT INTO recipe(author) VALUES ('$author')";
 	
 	 if(mysqli_query($db_handle, $sql)){
         echo "Username added successfully.";
     } else{
-        echo "ERROR" . mysqli_error($link);
+        echo "ERROR";
     }
 	
-	print "the name is: ";
-	print $u_name;
+	
+	
 	?>
 	
 </body>	
