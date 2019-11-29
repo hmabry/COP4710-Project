bf8f011907ade95a7f6d380117c13b2202a98438
heaven <hfm16@my.fsu.edu>
2019-11-28 21:17:16 -0500

    Finished basic recipe insertions -> all data is retrieved from forms in the HTML. Will format later.

--- index.php
+++ index.php
@@ -6,9 +6,55 @@
 <body>
 
 	<form method="post" action="index.php">
-	Username: <input type="text" name = "user_name" placeholder="Enter your username" /><br />
+	<label for="rname">Recipe Name</label>
+	<input type="text" id="rname" name="recipe_name" placeholder="Enter the recipe name" 
+	
+	<label for="aname">Author</label>
+	<input type="text" id="aname" name="author_name" placeholder="Enter your name" 
+	
+	<label for="pname">Prep Time (In Minutes)</label>
+	<input type="text" id="pname" name="prep_time" placeholder="Enter prep time" /><br />
+	
+	Ingredients: <br />
+	
+	<label for="iname">Ingredient Name</label>
+	<input type="text" id="iname" name="ingredient" placeholder="Enter ingredient name" /><br />
+	
+	<label for="istyle">Style</label>
+	<input type="text" id="iname" name="style" placeholder="Enter ingredient style" /><br />
+	
+	<label for="iamount">Amount</label>
+	<input type="text" id="iname" name="amount" placeholder="Enter ingredient amount" /><br />
+	
+	<label for="iunit">Unit</label>
+	<input type="text" id="iunit" name="unit" placeholder="Enter ingredient unit" /><br />
+	
+	Nutritional Info: <br />
+	
+	<label for="f_amt">Fat</label>
+	<input type="text" id="f_amt" name="fat" placeholder="Enter ingredient name" /><br />
+	
+	<label for="sod_amt">Sodium</label>
+	<input type="text" id="sod_amt" name="sodium" placeholder="Enter ingredient style" /><br />
+	
+	<label for="c_amt">Carbs</label>
+	<input type="text" id="c_amt" name="carbs" placeholder="Enter ingredient amount" /><br />
+	
+	<label for="p_amt">Protein</label>
+	<input type="text" id="p_amt" name="protein" placeholder="Enter ingredient amount" /><br />
+	
+	<label for="s_amt">Sugar</label>
+	<input type="text" id="s_amt" name="sugar" placeholder="Enter ingredient unit" /><br />
+	
+	Directions: <br />
+	
+	<label for="dirs">Directions</label>
+	<input type="text" id="dirs" name="directions" placeholder="Enter directions..." /><br />
+	
 	<input type="submit" value="Submit" />
 	</form>
+	
+
 
 	<?php
 	//Here I'm including the db connection file so I don't have to keep connecting in each file
@@ -17,34 +63,72 @@
 	$db_found = mysqli_select_db($db_handle, $database);
 
 	if($db_found){
-		$SQL = "SELECT * FROM user";
+		$SQL = "SELECT * FROM recipe";
 		$result = mysqli_query($db_handle, $SQL);
 		
 		while ( $db_field = mysqli_fetch_assoc($result) ) {
 
 		print "<BR>";
-		print $db_field['u_id'] . "<BR>";
-		print $db_field['username'] . "<BR>";
-		print $db_field['password'] . "<BR>";
+		print $db_field['name'] . "<BR>";
+		print $db_field['author'] . "<BR>";
 		}
 	}
 	else{
 		print "Database not found";
 	}
 	
-	$u_name = $_POST['user_name'];	
-
+	$r_name = $_POST['recipe_name'];	
+	$a_name = $_POST['author_name'];
+	$p_time = $_POST['prep_time'];	
 	
-	$sql = "INSERT INTO user(username) VALUES ('$u_name')";
+	$recipe_sql = "INSERT INTO recipe(name, author, preptime) VALUES ('$r_name', '$a_name', '$p_time')";
 	
-	 if(mysqli_query($db_handle, $sql)){
-        echo "Username added successfully.";
+	 if(mysqli_query($db_handle, $recipe_sql)){
+        echo "Recipe info added successfully.";
     } else{
         echo "ERROR" . mysqli_error($link);
     }
 	
 	print "the name is: ";
-	print $u_name;
+	print $r_name;
+	
+	$i_name = $_POST['ingredient'];	
+	$i_style = $_POST['style'];
+	$i_amount = $_POST['amount'];	
+	$i_unit = $_POST['unit'];	
+	
+	$ingredient_sql = "INSERT INTO ingredient(name, style, amount, unit) VALUES ('$i_name', '$i_style', '$i_amount', '$i_unit')";
+	
+	 if(mysqli_query($db_handle, $ingredient_sql)){
+        echo "Ingredient info added successfully.";
+    } else{
+        echo "ERROR" . mysqli_error($link);
+    }
+	
+	$fat = $_POST['fat'];	
+	$sodium = $_POST['sodium'];
+	$carbs = $_POST['carbs'];
+	$protein = $_POST['protein'];	
+	$sugar = $_POST['sugar'];	
+	
+	$nutrition_sql = "INSERT INTO nutrition(fat, sodium, carbs, protein, sugar) VALUES ('$fat', '$sodium', '$carbs', '$protein', '$sugar')";
+	
+	if(mysqli_query($db_handle, $nutrition_sql)){
+        echo "Nutritional info added successfully.";
+    } else{
+        echo "ERROR" . mysqli_error($link);
+    }
+	
+	$dirs = $_POST['directions'];
+	
+	$dirs_sql = "INSERT INTO recipe(directions) VALUES ('$dirs')";
+	
+	if(mysqli_query($db_handle, $dirs_sql)){
+        echo "Direction info added successfully.";
+    } else{
+        echo "ERROR" . mysqli_error($link);
+    }
+	
 	?>
 	
 </body>	
